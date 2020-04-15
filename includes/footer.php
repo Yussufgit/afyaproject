f49956e513bd829d9a2f419425e1817423dc35a9
yussufgit <yussufmohamed608@gmai.com>
2020-04-15 23:13:31 +0300

    commit on master

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+ <!-- footer -->
+		  <div class="footer">
+			<div class="wthree-copyright">
+			  <p>© 2020 Afya Project. All rights reserved | Designed by <a href="github.com/Yussufgit">Yussuf Abdiwali</a></p>
+			</div>
+		  </div>
+  <!-- / footer -->
+</section>
+<!--main content end-->
+</section>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/jquery.slimscroll.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
+<script src="js/jquery.scrollTo.js"></script>
+</body>
+</html>
